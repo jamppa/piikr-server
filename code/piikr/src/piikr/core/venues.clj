389070aa569615579943b928329b32719ab22cec
Jani Arvonen <jani.arvonen@gmail.com>
2015-02-21 17:34:11 +0200

    introduced ns for piikr.core.venues

---
+++ code/piikr/src/piikr/core/venues.clj
@@ -0,0 +1,5 @@
+(ns piikr.core.venues
+  (:require
+    [piikr.clients.foursquare :as fs]
+    [piikr.clients.instagram :as insta]))
+
---
+++ code/piikr/test/piikr/core/venues_test.clj
@@ -0,0 +1,5 @@
+(ns piikr.core.venues-test
+  (:use midje.sweet)
+  (:require
+    [piikr.clients.foursquare :as fs]
+    [piikr.clients.instagram :as insta]))
