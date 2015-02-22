6763716849e81528ee35b3d8a51f1c46c62d966b
Jani Arvonen <jani.arvonen@gmail.com>
2015-02-22 12:33:03 +0200

    core.venues - should find venues near with recent media

--- code/piikr/src/piikr/core/venues.clj
+++ code/piikr/src/piikr/core/venues.clj
@@ -3,3 +3,13 @@
     [piikr.clients.foursquare :as fs]
     [piikr.clients.instagram :as insta]))
 
+(defn- with-insta-location [venues]
+  (map #(insta/with-location-id %) venues))
+
+(defn- with-recent-insta-media [venues]
+  (map #(insta/with-recent-media %) venues))
+
+(defn find-near [lon lat]
+  (-> (fs/explore-venues-near lon lat)
+    with-insta-location
+    with-recent-insta-media))
--- code/piikr/test/piikr/core/venues_test.clj
+++ code/piikr/test/piikr/core/venues_test.clj
@@ -1,5 +1,16 @@
 (ns piikr.core.venues-test
   (:use midje.sweet)
   (:require
+    [piikr.core.venues :as venues]
     [piikr.clients.foursquare :as fs]
     [piikr.clients.instagram :as insta]))
+
+(def fs-venue {:id "123qwe"})
+(def venue-with-recent-media (merge fs-venue {:media {}}))
+
+(fact "should find venues with recent media near location"
+  (venues/find-near 60.19 24.45) => [venue-with-recent-media]
+  (provided
+    (fs/explore-venues-near 60.19 24.45) => [fs-venue] :times 1
+    (insta/with-location-id fs-venue) => fs-venue :times 1
+    (insta/with-recent-media fs-venue) => venue-with-recent-media :times 1))
